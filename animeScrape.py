142c1f85712d65582e9c4a7d13f170738b77660c
Kurtis David <kurtis.david@live.com>
2017-06-22 13:44:27 -0500

    fixed 1 page bug + added comments

--- animeScrape.py
+++ animeScrape.py
@@ -41,9 +41,13 @@ def genreCollect(link, foundAnime):
     if not os.path.exists('./' + genre):
         os.makedirs(directory)
 
-    # find number of pages of anime to scrape
-    n_pages = int(soup.find('div', class_ = 'pagination ac').span.nextSibling.get('href').split('=')[-1])
+    # try find number of pages of anime to scrape
+    try:
+        n_pages = int(soup.find('div', class_ = 'pagination ac').span.nextSibling.get('href').split('=')[-1])
+    except:
+        n_pages = 1
 
+    # collect anime from each page
     for i in range(1,n_pages+1):
 
         soup = BeautifulSoup(urllib.request.urlopen('https://myanimelist.net'+link+'?page='+str(i)).read(),'lxml')
@@ -51,30 +55,35 @@ def genreCollect(link, foundAnime):
         anime = soup.findAll('div', class_ = 'seasonal-anime js-seasonal-anime')
         for an in anime:
 
+            # title of anime
             title = an.div.div.find('a',class_ = 'link-title').get('href').split("/")[-1]
 
+            # don't collect any data if we've already taken it before
             if title in foundAnime:
-
                 continue
             
+            # collect all of the genres
             an_genres = []
             found = an.div.findAll('span', class_ = 'genre')
 
             for g in found:
                 an_genres.append(g.a.get('title'))
 
+            # convert to a string to be written
             an_genres = ';'.join(an_genres)
 
-            # get synopsis and get rid of unneeded part of summary **Make sure to credit MAL Rewrite**
+            # get synopsis
             synopsis = an.find('span', class_ = 'preline').getText()
-            synopsis = synopsis.replace('\n[Written by MAL Rewrite]', '')
 
+            # final output string
             final_output = an_genres + "\n" + synopsis
 
+            # write to file
             file = open("./" + genre + "/" + title + '.txt','w', encoding="utf-8")
             file.write(final_output)
             file.close()
 
+            # add to hash table
             foundAnime[title] = 1
 
     return foundAnime
